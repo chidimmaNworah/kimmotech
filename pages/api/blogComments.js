5cb52ff9000b0cac8c0f78e3c2f861a9a96eba42
kimmoramicky <kimmoramicky@localhost.localdomain>
2022-09-19 12:44:29 +0100

    t

--- pages/api/blogComments.js
+++ pages/api/blogComments.js
@@ -3,7 +3,7 @@
 import { GraphQLClient, gql } from "graphql-request"
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
-const graphcmsToken = process.env.GRAPHCMS_TOKEN2
+const graphcmsToken = process.env.GRAPHCMS_TOKEN
 
 export default async function blogComments(req, res){
   const {name, email, slug, comment} = req.body
